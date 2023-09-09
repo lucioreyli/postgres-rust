c56c5c96e674744d970942645be35f37d7d8d185
Lúcio Andrade <lucioandradejr@gmail.com>
2023-09-09 03:01:45 -0400

    feat: finish query

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-use postgres::{Client, NoTls};
+use openssl::ssl::{SslConnector, SslMethod};
+use postgres::Client;
+use postgres_openssl::MakeTlsConnector;
 
-fn main() {
-    const _CON_STR: &str = "host=localhost user=postgres password=abacaxi";
+const CON_STR: &str = "postgres://postgres:abacaxi@localhost:5432/postgres";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let builder = SslConnector::builder(SslMethod::tls())?;
+    let connector = MakeTlsConnector::new(builder.build());
+    let mut client = Client::connect(CON_STR, connector)?;
+
+    let result = client.query(
+        "
+SELECT tablename FROM pg_tables WHERE schemaname = 'public';
+        ",
+        &[],
+    )?;
+
+    for row in result {
+        let name: &str = row.get(0);
+
+        println!("{}", name);
+    }
+
+    let _ = client.close();
+
+    Ok(())
 }
